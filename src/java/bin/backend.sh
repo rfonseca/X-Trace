db5f6d4b5b936e164721f06c912afa09cd26b9ea
gporter <gporter@63ced123-6a40-0410-8707-b735ea0ac295>
2008-03-23 18:50:12 +0000

    Some refactoring for the 2.0 release
    git-svn-id: https://xtraceprj.svn.sourceforge.net/svnroot/xtraceprj/trunk@43 63ced123-6a40-0410-8707-b735ea0ac295

---
+++ src/java/bin/backend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASE=`dirname ${0}`/..
+
+CLASSPATH=$BASE/lib/derby.jar:$BASE/lib/junit-4.4.jar:$BASE/lib/velocity-dep-1.5.jar:$BASE/lib/je-3.2.44.jar:$BASE/lib/log4j-1.2.15.jar:$BASE/lib/xtrace-2.0.jar:$BASE/lib/jetty-6.1.6.jar:$BASE/lib/servlet-api-2.5-6.1.6.jar:$BASE/lib/jetty-util-6.1.6.jar:$BASE/lib/velocity-1.5.jar:$BASE/lib/libthrift.jar:$BASE
+
+java -Dlog4j.configuration=lib/log4j-server.properties -Dxtrace.backend.webui.dir=$BASE/webui -cp $CLASSPATH edu.berkeley.xtrace.server.XTraceServer $BASE/data
---
+++ src/java/bin/proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -u
+
+if [ $# -ne 1 ]; then
+	echo 1>&2 Usage: $0 xtraceServerHostname
+	exit 127
+fi
+
+BASE=`dirname ${0}`
+
+CLASSPATH=$BASE/lib/derby.jar:$BASE/lib/junit-4.4.jar:$BASE/lib/velocity-dep-1.5.jar:$BASE/lib/je-3.2.44.jar:$BASE/lib/log4j-1.2.15.jar:$BASE/lib/xtrace-2.0.jar:$BASE/lib/jetty-6.1.6.jar:$BASE/lib/servlet-api-2.5-6.1.6.jar:$BASE/lib/jetty-util-6.1.6.jar:$BASE/lib/velocity-1.5.jar:$BASE/lib/libthrift.jar:$BASE
+
+java -Dlog4j.configuration=lib/log4j-proxy.properties -Dxtrace.reporter="edu.berkeley.xtrace.reporting.TcpReporter" -Dxtrace.tcpdest="${1}:7831" -cp $CLASSPATH edu.berkeley.xtrace.server.XTraceProxy

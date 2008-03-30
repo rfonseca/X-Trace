20fffd94350e6fe8bb80da885023c315427317c3
andykonwinski <andykonwinski@63ced123-6a40-0410-8707-b735ea0ac295>
2008-03-30 05:20:56 +0000

    refactoring
    git-svn-id: https://xtraceprj.svn.sourceforge.net/svnroot/xtraceprj/trunk@61 63ced123-6a40-0410-8707-b735ea0ac295

---
+++ docs/tutorials/tutorial1/chatapp/ChatMessage.java
@@ -0,0 +1,25 @@
+/* a simple protocol for including some metadata 
+ * with chat messages, such as time stamp
+ */
+package chatapp;
+
+import java.io.Serializable; 
+import java.util.Date;
+
+public class ChatMessage implements Serializable{
+	String message;
+	Date currDate;
+	//byte[] xtraceMD;
+	
+	public ChatMessage(){}
+	
+	public ChatMessage(String msg){
+		message = msg;
+		currDate = new Date();
+	}
+	
+	public String getTime(){
+		return currDate.getHours() + ":" + currDate.getMinutes() + ":" + currDate.getSeconds();
+	}
+	
+}

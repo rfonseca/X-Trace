8b98daf9c35b3ab1714f3421cdacb9b7cda854b3
andykonwinski <andykonwinski@63ced123-6a40-0410-8707-b735ea0ac295>
2008-04-03 23:31:44 +0000

    updated classpath to include lib for log4j property files for backend again
    git-svn-id: https://xtraceprj.svn.sourceforge.net/svnroot/xtraceprj/trunk@69 63ced123-6a40-0410-8707-b735ea0ac295

--- src/java/bin/backend.sh
+++ src/java/bin/backend.sh
@@ -2,6 +2,6 @@
 
 BASE=`dirname ${0}`/..
 
-CLASSPATH=$BASE/lib/xtrace-2.0.jar
+CLASSPATH=$BASE/lib/xtrace-2.0.jar:$BASE/lib/
 
 java -Dlog4j.configuration=log4j-server.properties -Dxtrace.backend.webui.dir=$BASE/webui -cp $CLASSPATH edu.berkeley.xtrace.server.XTraceServer $BASE/data

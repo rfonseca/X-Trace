1475322a3323391dbcfd533459c98205f8751c11
andykonwinski <andykonwinski@63ced123-6a40-0410-8707-b735ea0ac295>
2008-04-04 00:44:12 +0000

    added option for passing a directory, uses default if none is specified
    git-svn-id: https://xtraceprj.svn.sourceforge.net/svnroot/xtraceprj/trunk@70 63ced123-6a40-0410-8707-b735ea0ac295

--- src/java/bin/backend.sh
+++ src/java/bin/backend.sh
@@ -2,6 +2,23 @@
 
 BASE=`dirname ${0}`/..
 
+if [ $# != 0 ]; then
+  if [ $1 == "-h" ]; then
+    echo -e "Usage: backend.sh [X-Trace/data/dir]\nNOTE: default directory is ../data"
+    exit
+  else
+    if [ -d $1 ]; then  
+      DATA_DIR=$1
+    else
+      echo "WARNING: Invalid data directory provided, using default ($BASE/data) instead"
+      DATA_DIR=$BASE/data
+    fi
+  fi
+else
+  echo "Using default data directory: $BASE/data"
+  DATA_DIR=$BASE/data
+fi
+
 CLASSPATH=$BASE/lib/xtrace-2.0.jar:$BASE/lib/
 
-java -Dlog4j.configuration=log4j-server.properties -Dxtrace.backend.webui.dir=$BASE/webui -cp $CLASSPATH edu.berkeley.xtrace.server.XTraceServer $BASE/data
+java -Dlog4j.configuration=log4j-server.properties -Dxtrace.backend.webui.dir=$BASE/webui -cp $CLASSPATH edu.berkeley.xtrace.server.XTraceServer $DATA_DIR

1485d427fd3f94ef3b5352eafa7af24947368ade
Beth Trushkowsky <trush@eecs.berkeley.edu>
2009-06-04 10:55:31 -0700

    static vars for severity levels

---
+++ src/java/src/edu/berkeley/xtrace/OptionSeverity.java
@@ -0,0 +1,15 @@
+package edu.berkeley.xtrace;
+
+public class OptionSeverity {
+    public static final int    EMERG = 0;
+	public static final int    ALERT = 1;
+	public static final int CRITICAL = 2;
+	public static final int    ERROR = 3;
+	public static final int  WARNING = 4;
+	public static final int   NOTICE = 5;
+	public static final int     INFO = 6;
+	public static final int    DEBUG = 7;
+	public static final int     _ALL = 8;
+	public static final int    _NONE = 255;
+	public static final int  DEFAULT = NOTICE;
+}
